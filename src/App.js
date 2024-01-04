e04cee89197096580c4f14c2bf9e1f41670d6a9e
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2024-01-04 10:24:09 +0530

    added waifu page for 1 waifu on click andd styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Char from './components/Char';
 import { Routes,Route } from 'react-router-dom';
+import Waifu from './components/Waifu';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Char' element={<Char/>}/>
+       <Route path='/Waifu' element={<Waifu/>}/>
       </Routes>
     </div>
   );
--- src/components/Waifu.js
+++ src/components/Waifu.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
+import "../styles/waifu.css";
+import axios from 'axios';
 
 const Waifu = () => {
+  let myarr=[];
+  const [urls, setUrls] = useState(myarr);
+  //const [infield, setInfield] = useState("");
+ // const newInfield = infield.charAt(0).toUpperCase();
+ // const namestring=newInfield+infield.slice(1);
+  const handleClick = () => {
+    
+    const fetchanimedata = async () => {
+      try {
+        const mydata = await axios.get(`https://waifu.it/api/waifu`, {
+          headers: {
+            Authorization:
+              process.env.REACT_APP_ANIME_API_KEY,
+          },
+        });
+        console.log(mydata.data.images);
+        setUrls(...mydata.data.images);
+       // console.log(mynewData)
+       myarr=urls;
+
+      } catch (e) {
+        alert("Try some other theme pls");
+      }
+    };
+    fetchanimedata();
+  };
   return (
-    <div>Waifu</div>
+    <div className="waifumain">
+    <div>
+      {/* {myarr.map((ind)=>{
+        <img src={ind}/>
+      })} */}
+      <img className="waifuimg"src={urls} alt='some error occured'/>
+    </div>
+    <div>
+      <button className="navbutton" onClick={handleClick} type="button">
+        My Waifu
+      </button>
+    </div>
+  </div>
   )
 }
 
-export default Waifu
+export default Waifu;
